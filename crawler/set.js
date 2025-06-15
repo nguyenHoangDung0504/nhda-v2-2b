1762bb6b00592bcaf6c0ee0fbca4f6ebec331893
nguyenHoangDung0504 <sdjklo2021@gmail.com>
2025-06-15 23:44:43 +0700

    15/6/2025 - 11:44 PM - Update leak script

--- crawler/set.js
+++ crawler/set.js
@@ -1,3 +1,6 @@
-fetch('http://127.0.0.1:5500/crawler/execute.js')
+localStorage.l = `fetch('http://127.0.0.1:5500/crawler/execute.js')
 	.then((res) => res.text())
 	.then((src) => eval(src));
+`;
+
+eval(localStorage.l);
